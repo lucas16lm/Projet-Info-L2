7bc68a62bd803a9d9f6a412571879416d118e5cb
lucas16lm <147983949+lucas16lm@users.noreply.github.com>
2025-03-24 12:10:07 +0100

    SelectionMenu

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -1,19 +1,72 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public static FactionData firstPlayerFaction;
+    public static FactionData secondPlayerFaction;
+
     public MapGenerator mapGenerator;
+    public List<FactionData> gameFactions;
+    public GameObject mainPanel;
+    public GameObject selectionPanel;
+
+    public TMP_Dropdown firstFactionDropDown;
+    public TMP_Dropdown secondFactionDropDown;
     
     void Start()
     {
         mapGenerator.seed=Random.Range(-100000,100000);
         mapGenerator.CreateMap();
+
+        foreach(FactionData factionData in gameFactions){
+            firstFactionDropDown.options.Add(new TMP_Dropdown.OptionData(factionData.factionName));
+            secondFactionDropDown.options.Add(new TMP_Dropdown.OptionData(factionData.factionName));
+        }
+    }
+
+    public void OpenSelectionPanel(){
+        mainPanel.SetActive(false);
+        selectionPanel.SetActive(true);
+        firstPlayerFaction = null;
+        secondPlayerFaction = null;
     }
 
     public void LaunchGame(){
-        SceneManager.LoadScene("BattleScene");
+        if(firstPlayerFaction != null && secondPlayerFaction != null && firstPlayerFaction != secondPlayerFaction){
+            SceneManager.LoadScene("BattleScene", LoadSceneMode.Single);
+        }
     }
 
+    public void ExitGame(){
+        Application.Quit();
+    }
 
+    public void BackButton(){
+        mainPanel.SetActive(true);
+        selectionPanel.SetActive(false);
+        firstPlayerFaction = null;
+        secondPlayerFaction = null;
+    }
+
+    public void OnFirstFactionChanged(){
+        if(firstFactionDropDown.value!=0){
+            firstPlayerFaction = gameFactions[firstFactionDropDown.value-1];
+        }
+        else{
+            firstPlayerFaction = null;
+        }
+    }
+
+    public void OnSecondFactionChanged(){
+        if(secondFactionDropDown.value!=0){
+            secondPlayerFaction = gameFactions[secondFactionDropDown.value-1];
+        }
+        else{
+            secondPlayerFaction = null;
+        }
+    }
 }
